efffd9ede5e1da0054d2c68334b7a9ee84f4d7e1
XU Xiaohchuan <35758677+incandescentxxc@users.noreply.github.com>
2018-11-19 23:10:58 +0800

    finish the function

--- upload.php
+++ upload.php
@@ -1,6 +1,45 @@
 <?php
 	if(!isset($_COOKIE["username"]))
 		header("Location: login.php");
+	$email = $_COOKIE["email"];
+	
+	if(isset($_POST["submit"])){
+	if($_FILES["file"]["error"] > 0)
+	{
+		echo "Error: " .$_FILES["file"]["error"]."<br/>";
+	}
+	else
+	{
+		$description = $_POST["description"];
+	
+		$file_name = $_FILES["file"]["name"];
+		$str1 = strchr($file_name,".");
+		if($str1){
+			$file_name = trim($file_name,$str1);/*used to obtain the file name without the postfix*/}
+		move_uploaded_file($_FILES["file"]["tmp_name"],"uploadVideos/" . $_FILES["file"]["name"]);
+		$db = mysql_connect("sdmysql.comp.polyu.edu.hk","18012633x","sqgqcbvd");
+		if(!$db)
+		{
+			die('Could not connect: ' .mysql_error());
+		}
+		
+		mysql_select_db("18012633x",$db);
+		$sql = "INSERT INTO vocabulary(submitter,approver,status,vocabName,description,videoSource,checkTotal,addTotal)
+						VALUES( '$email','1767182376@qq.com' ,'unapproved','$file_name','$description','upload/','0','0')";
+		
+		if(!mysql_query($sql,$db)){
+			die('Error: '.mysql_error());
+		}
+		
+		echo "1 video added";
+		mysql_close($db);
+		
+		
+	}
+	}
+									  
+									 
+	
 ?>
 
 <!doctype html>
@@ -22,7 +61,18 @@ $(document).ready(function(){
 	<?php require('header.php');?>
 	
 	<div class="mainFrame">
-		待开发。。。
+		Please upload your video:
+		<form action="upload.php" method = "post" enctype="multipart/form-data">
+        <label for = "file">Filename:</label>
+        <input type = "file" name = "file" id = "file"/>
+        <br/>
+        Please use words to explain the meaning of this word:
+        <input type = "text" name = "description"/>
+        <br/>
+        <input type = "submit" name = "submit" value="Submit"/>
+        </form>
+	
+		
 	</div>
 	
 	<?php require('footer.php');?>
